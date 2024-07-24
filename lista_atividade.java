52beddf5b0f82a569ab8d10144717efff21c8f07
Pedro Henrique <166336814+Pedro9820@users.noreply.github.com>
2024-07-24 09:04:28 -0300

    Atualizar o lista_atividade.java

--- lista_atividade.java
+++ lista_atividade.java
@@ -4,7 +4,7 @@ public class lista_atividade {
 Private Arraylist<atividade> lista;
 
 Public void adicionaratvd(){}
-Public void excluirAtvd(int){}
+Public void excluirAtvd(int num){}
 Public void editarAtvd(int num, atividade atv){}
 
 }
