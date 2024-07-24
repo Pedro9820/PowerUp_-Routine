73f40b5421d2754e88dce7edc7e1fe7fba5f4450
Pedro Henrique <166336814+Pedro9820@users.noreply.github.com>
2024-07-24 09:02:21 -0300

    Atualizar o atividade.java

--- atividade.java
+++ atividade.java
@@ -1,2 +1,10 @@
+import java.time.LocalDate;
 public class atividade {
+
+Private String id, nome, tipo;
+Private int intensidade;
+Private LocalDateTime horarioInicio, horarioTermino;
+
+Public void calcularAumentoAtributo(){}
+
 }
