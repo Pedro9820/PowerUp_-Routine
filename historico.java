d7393d967ecde49fe6a18cb8a99eb841b9ce860f
Pedro Henrique <166336814+Pedro9820@users.noreply.github.com>
2024-07-24 09:03:01 -0300

    Atualizar o historico.java

--- historico.java
+++ historico.java
@@ -1,2 +1,10 @@
+import java.time.LocalDate;    
 public class historico {
+
+Private LocalDate data;
+Private boolean foiConcluida;
+
+Public void adicionarAtividade(Atividade){}
+Public void removerAtividade(Atividade){}
+
 }
