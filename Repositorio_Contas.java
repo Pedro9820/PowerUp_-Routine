0b6770fc5b1e2a464d25a87eca12577c84ef9eb7
Pedro Henrique <166336814+Pedro9820@users.noreply.github.com>
2024-07-24 14:17:28 -0300

    Atualizar o Repositorio_Contas.java

--- Repositorio_Contas.java
+++ Repositorio_Contas.java
@@ -1,9 +1,16 @@
 import java.util.ArrayList;
 public class Repositorio_Contas {
 
-    private ArrayList<Conta> contas=contas= new ArrayList<>();;
+    private ArrayList<Conta> contas=contas= new ArrayList<>();
 
     public void adicionar_conta(Conta conta){
         contas.add(conta);
     }
+    public void remover_conta(String id){
+        contas.remove(id);
+}
+    public void editar_user( String id, Atividade atividade){
+        
+
+}
 }
