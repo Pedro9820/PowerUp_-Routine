135d45249defd1f6aa98df66b7bfbd8a92d0889c
Pedro <pedro.apolinario@ufrpe.br>
2024-08-18 16:42:10 -0300

    controladores

---
+++ src/br/ufrpe/powerUp/controllers/controladorObjetivo.java
@@ -0,0 +1,44 @@
+package src.br.ufrpe.powerUp.controllers;
+
+import src.br.ufrpe.powerUp.beans.Objetivo;
+
+public class controladorObjetivo {
+    private Objetivo objetivo;
+
+    public controladorObjetivo(Objetivo objetivo) {
+        this.objetivo = objetivo;
+    }
+    public int getObjetivoId() {
+        return objetivo.getId();
+    }
+
+    public String getObjetivoNome() {
+        return objetivo.getNome();
+    }
+
+    public String getObjetivoDescricao() {
+        return objetivo.getDescricao();
+    }
+
+    public int getObjetivoQuota() {
+        return objetivo.getQuota();
+    }
+
+    public void setObjetivoId(int id) {
+        this.objetivo.setId(id);
+    }
+
+    public void setObjetivoNome(String nome) {
+        this.objetivo.setNome(nome);
+    }
+
+    public void setObjetivoDescricao(String descricao) {
+        this.objetivo.setDescricao(descricao);
+    }
+
+    public void setObjetivoQuota(int quota) {
+        this.objetivo.setQuota(quota);
+    }
+
+
+}
---
+++ src/br/ufrpe/powerUp/controllers/controladorUsuario.java
@@ -0,0 +1,83 @@
+package src.br.ufrpe.powerUp.controllers;
+
+import src.br.ufrpe.powerUp.beans.Objetivo;
+import src.br.ufrpe.powerUp.beans.TipoAtributo;
+import src.br.ufrpe.powerUp.beans.Usuario;
+
+public class controladorUsuario {
+    private Usuario usuario;
+
+
+    public controladorUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getUsuarioName(){
+        return usuario.getNome();
+    }
+
+    public int getUsuarioForca(){
+        return usuario.getForca();
+    }
+
+    public int getUsuarioResistencia(){
+        return usuario.getResistencia();
+    }
+
+    public int getUsuarioStamina(){
+        return usuario.getStamina();
+    }
+
+    public int getUsuarioVelocidade(){
+        return usuario.getVelocidade();
+    }
+
+    public float getUsuarioPeso(){
+        return usuario.getPeso();
+    }
+
+    public float getUsuarioAltura(){
+        return usuario.getAltura();
+    }
+
+    public void atualizarAtributoUsuario(TipoAtributo tipo, int qtd){
+        this.usuario.atualizar_atributo(tipo, qtd);
+    }
+
+    public void adicionarObjetivoUsuario(Objetivo objetivo){
+        this.usuario.adicionar_objetivo(objetivo);
+    }
+
+    public void setForcaUsuario(int valor){
+        this.usuario.setForca(valor);
+    }
+
+    public void setResistenciaUsuario(int valor){
+        this.usuario.setResistencia(valor);
+    }
+
+    public void setStaminaUsuario(int valor){
+        this.usuario.setStamina(valor);
+    }
+
+    public void setVelocidadeUsuario(int valor){
+        this.usuario.setVelocidade(valor);
+    }
+
+    public void setPesoUsuario(float valor){
+        this.usuario.setPeso(valor);
+    }
+
+    public void setAlturaUsuario(float valor){
+        this.usuario.setAltura(valor);
+    }
+
+    public void setNomeUsuario(String nome){
+        this.usuario.setNome(nome);
+    }
+
+    public void setIdUsuario(String id){
+        this.usuario.setID(id);
+    }
+
+}
